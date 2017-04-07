f73be37a3ce45eea2bab269dad2767188e7a4e91
spawn150 <spawn150@gmail.com>
2017-04-07 19:01:40 +0200

    implementato esempio ANR

--- app/src/main/java/it/englab/androidcourse/threads/MainActivity.java
+++ app/src/main/java/it/englab/androidcourse/threads/MainActivity.java
@@ -1,8 +1,13 @@
 package it.englab.androidcourse.threads;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import it.englab.androidcourse.threads.anr.ANRActivity;
+import it.englab.androidcourse.threads.thread.ThreadActivity;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_anr).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ANRActivity.class));
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/it/englab/androidcourse/threads/anr/ANRActivity.java
@@ -0,0 +1,41 @@
+package it.englab.androidcourse.threads.anr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import it.englab.androidcourse.threads.R;
+
+public class ANRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anr);
+
+        findViewById(R.id.button_start_thread).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                anrExample();
+            }
+        });
+        findViewById(R.id.button_secondary).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ANR", "Vorrei fare altro!");
+            }
+        });
+    }
+
+
+    private void anrExample() {
+        try {
+            Thread.sleep(20000);
+            Toast.makeText(getApplicationContext(), "Il nostro fantastico codice ha finito!", Toast.LENGTH_SHORT).show();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
