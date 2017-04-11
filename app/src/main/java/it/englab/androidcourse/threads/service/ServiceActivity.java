785c9b1bac3e9f7a30d770d2e4705ac6f5541fd2
spawn150 <spawn150@gmail.com>
2017-04-11 23:02:02 +0200

    aggiunta Activity per esempio con il Service

---
+++ app/src/main/java/it/englab/androidcourse/threads/service/ServiceActivity.java
@@ -0,0 +1,15 @@
+package it.englab.androidcourse.threads.service;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.englab.androidcourse.threads.R;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+    }
+}
